5ec7ae5064b01599b9e45481846cabada94bb7ed
Angel Camargo <adcamargo10@gmail.com>
2023-03-08 17:53:50 -0400

    first commit Email Project BOOTCAMP JavaScript

---
+++ index.js
@@ -0,0 +1,63 @@
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_adress: req.body.email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: req.body.firstName,
+                    LNAME: req.body.lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/251d3eeeab";
+
+    const options = {
+        method: "POST",
+        auth: "acamargo:fea71395be22396c16f2737a86855cf2-us21"
+    };
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data) {
+             console.log(JSON.parse(data));
+        });
+    });
+    
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.listen(3000, function () {
+    console.log('Server is running on port 3000');
+});
+
+// API KEY
+// fea71395be22396c16f2737a86855cf2-us21
+// AUDIENCE ID
+// 251d3eeeab
